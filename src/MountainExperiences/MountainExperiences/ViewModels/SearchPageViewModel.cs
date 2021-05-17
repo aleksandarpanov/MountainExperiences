805f123b8cbf51475c094dd0d425513d8e91c6bd
Aleksandar Panov <aleksandar.panov@rovicom.net>
2021-05-17 14:19:42 +0200

    Main page - search page navigation

---
+++ src/MountainExperiences/MountainExperiences/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MountainExperiences.ViewModels
+{
+    public class SearchPageViewModel : ViewModelBase
+    {
+        public SearchPageViewModel(INavigationService navigationService)
+            :base(navigationService)
+        {
+
+        }
+
+        private ICommand goBackCommand;
+
+        public ICommand GoBackCommand
+        {
+            get
+            {
+                if (goBackCommand == null)
+                {
+                    goBackCommand = new DelegateCommand(async () => await GoBackAsync());
+                }
+
+                return goBackCommand;
+            }
+        }
+
+        private async Task GoBackAsync()
+        {
+            INavigationResult navigationResult = await NavigationService.GoBackAsync();
+
+            if (!navigationResult.Success)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+    }
+}
