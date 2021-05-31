8092669111d48ecd8a1f4ffd5765e6663abd2c57
Aleksandar Panov <aleksandar.panov@gmail.com>
2021-05-31 21:20:55 +0200

    home page - android, ready

---
+++ src/MountainExperiences/MountainExperiences.Android/Effects/RemoveEntryUnderline.cs
@@ -0,0 +1,31 @@
+﻿using Android.Graphics;
+using Android.Widget;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("XFBaseEntry.RemoveEntryUnderline")]
+[assembly: ExportEffect(typeof(MountainExperiences.Droid.Effects.RemoveEntryUnderline), nameof(MountainExperiences.Droid.Effects.RemoveEntryUnderline))]
+
+namespace MountainExperiences.Droid.Effects
+{
+    public class RemoveEntryUnderline : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            var editText = this.Control as EditText;
+
+            if (editText is null)
+            {
+                throw new NotImplementedException();
+            }
+
+            editText.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+
+        protected override void OnDetached()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
