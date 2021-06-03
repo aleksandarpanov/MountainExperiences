d22bb680c7e44ebe69fd0fa2d924c07bcb8d8df2
Aleksandar Panov <aleksandar.panov@gmail.com>
2021-06-03 20:07:32 +0200

    ios adjustments

--- src/MountainExperiences/MountainExperiences/Views/MainPage.xaml.cs
+++ src/MountainExperiences/MountainExperiences/Views/MainPage.xaml.cs
@@ -1,11 +1,23 @@
 ﻿
+using System;
+using Xamarin.Essentials;
+
 namespace MountainExperiences.Views
 {
     public partial class MainPage
     {
+        double density;
         public MainPage()
         {
             InitializeComponent();
+            // Screen density
+            density = DeviceDisplay.MainDisplayInfo.Density;
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            VerticalDottedLine.Y2 = 0.65 * ParentGrid.Height;
         }
     }
 }
